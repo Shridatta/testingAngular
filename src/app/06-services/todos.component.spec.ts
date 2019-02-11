b8f13f5bc0464a3559fe952f2687d2111fa596ff
Shridatta <sgz2@njit.edu>
2019-02-10 23:42:26 -0500

    testing crud

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -2,6 +2,8 @@ import { TodosComponent } from "./todos.component";
 import { TodoService } from "./todo.service";
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/observable/from";
+import "rxjs/add/observable/empty";
+import "rxjs/add/observable/throw";
 
 describe("TodosComponent", () => {
   let component: TodosComponent;
@@ -19,5 +21,43 @@ describe("TodosComponent", () => {
     });
     component.ngOnInit();
     expect(component.todos).toBe(todos);
+  });
+
+  it("should call the server to save the chnages when a new todo item is added", () => {
+    let spy = spyOn(service, "add").and.callFake(t => {
+      return Observable.empty();
+    });
+    component.add();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it("should add the new Todo returned from the server", () => {
+    let todo = { id: 1 };
+    let spy = spyOn(service, "add").and.callFake(t => {
+      return Observable.from([todo]);
+    });
+    component.add();
+    expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
+  });
+
+  it("should set the message property if server sends errors while adding a new Todo ", () => {
+    let error = "Error from the server";
+    let spy = spyOn(service, "add").and.returnValue(Observable.throw(error));
+    component.add();
+    expect(component.message).toBe(error);
+  });
+
+  it("should call the server to delete if user presses confirm ", () => {
+    spyOn(window, "confirm").and.returnValue(true);
+    let spy = spyOn(service, "delete").and.returnValue(Observable.empty());
+    component.delete(1);
+    expect(spy).toHaveBeenCalledWith(1);
+  });
+
+  it("should NOT call the server to delete if user presses cancel ", () => {
+    spyOn(window, "confirm").and.returnValue(false);
+    let spy = spyOn(service, "delete").and.returnValue(Observable.empty());
+    component.delete(1);
+    expect(spy).not.toHaveBeenCalled();
   });
 });
